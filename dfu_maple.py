defa8150acdbfcc940a71f553915d54a7ea6b87d
Celian Riboulet <celian.riboulet@gmail.com>
2020-12-15 19:40:52 +0100

    dfu_mapple.py corrigé pour upload sur bootloader

--- dfu_maple.py
+++ dfu_maple.py
@@ -2,10 +2,10 @@ import os
 Import("env")
 
 uploader = '$UPLOADER'
-local_dfu = ['dfu-util', 'dfu-util.exe']
-for dfu in local_dfu:
-    if os.path.exists(dfu):
-        uploader = './'+dfu
+#local_dfu = ['dfu-util', 'dfu-util.exe']
+#for dfu in local_dfu:
+#    if os.path.exists(dfu):
+#        uploader = './'+dfu
 
 # DFU util to program the Maple Mini
 env.Replace(
