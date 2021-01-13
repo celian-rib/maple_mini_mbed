8ce9ce9efb7d5a8079cee83c274f928ef42e8082
celian-rib <celian.riboulet@gmail.com>
2021-01-13 12:23:49 +0100

    Blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <mbed.h>
 #include <USBSerial.h>
 
-DigitalOut led(PB_1);
+/*
+  Digital out : créer un port (une broche de sortie)
+
+    - DigitalOut nomvariable(port);
+*/
+DigitalOut led(PA_7);//Ici il y a une led branchée sur le pin 4 de la mapple mini
+
 DigitalOut usbDisconnect(PB_9);
 
 int main()
 {
-    USBSerial usbSerial;
-    usbDisconnect = 0;
+    //USBSerial usbSerial;
+    usbDisconnect = 0;//Pour que l'usb soit reconnu avec la mapple mini
 
     while (true) {
-        usbSerial.printf("hello\r\n");
-        led = 0;
-        ThisThread::sleep_for(500);
-        led = 1;
-        ThisThread::sleep_for(500);
+         led = 1;//Allumage de la led
+        ThisThread::sleep_for(200);//Attente de 200 millisecondes
+         led = 0;//Extinction de la led
+        ThisThread::sleep_for(200);
     }
 }
